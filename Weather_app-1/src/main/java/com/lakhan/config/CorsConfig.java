503dd1232e68ea85d6969d9559d74dacb6e8fa5d
Lakhan Gurnule <101392912+Lakhan1122@users.noreply.github.com>
2024-04-08 22:19:27 +0530

    Update CorsConfig.java

--- Weather_app-1/src/main/java/com/lakhan/config/CorsConfig.java
+++ Weather_app-1/src/main/java/com/lakhan/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("*") // Replace this with your frontend URL
+                .allowedOrigins("*") // for all server use
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
