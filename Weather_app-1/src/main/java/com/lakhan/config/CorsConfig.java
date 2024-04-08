7f162dcb6e7f4eb07f4a0542b8aaa944534b0f06
Lakhan Gurnule <101392912+Lakhan1122@users.noreply.github.com>
2024-04-08 22:26:10 +0530

    Update CorsConfig.java

--- Weather_app-1/src/main/java/com/lakhan/config/CorsConfig.java
+++ Weather_app-1/src/main/java/com/lakhan/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.lakhan.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
